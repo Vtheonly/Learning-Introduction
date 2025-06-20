e46abfa2ed7f9b5a247f28112d93f8ce47a55055
mersel fares <merselfaresw@gmail.com>
2025-06-21 00:16:04 +0100

    Cleaned nested Git folders and refreshed tracking

--- C++/replica-of-lists/listObject.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include <Linkedlist.cpp>
-using namespace std;
-class List{
-    Node* x= createLinkedList(3,5);
-    // ====================
-    Node* y= createLinkedList(2,15);
-};
